421e601f089f0ac712a9b2bc9d04279b87a8ea9a
cesmiley <catherinesmiley@gmail.com>
2020-07-29 12:53:22 -0400

    ran generator and migration for new word model

--- rails-js-project-backend/config/routes.rb
+++ rails-js-project-backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :words
   resources :games
   resources :users
   resources :small_words
--- rails-js-project-backend/db/schema.rb
+++ rails-js-project-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_164749) do
+ActiveRecord::Schema.define(version: 2020_07_29_165211) do
 
   create_table "big_words", force: :cascade do |t|
     t.string "name"
@@ -40,5 +40,14 @@ ActiveRecord::Schema.define(version: 2020_07_29_164749) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "words", force: :cascade do |t|
+    t.string "name"
+    t.integer "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_words_on_game_id"
+  end
+
   add_foreign_key "small_words", "big_words"
+  add_foreign_key "words", "games"
 end
