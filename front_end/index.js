ecc20933039864fe37adc3fb741d4a6c1664e365
cesmiley <catherinesmiley@gmail.com>
2020-07-28 21:34:16 -0400

    set base url and created renderFirstWord function

--- front_end/index.js
+++ front_end/index.js
@@ -1,3 +1,11 @@
+const BASE_URL = "http://localhost:3000/"
+
 document.addEventListener("DOMContentLoaded", () => {
-    
+    renderFirstWord()
 })
+
+function renderFirstWord() {
+    // add clear form function
+    let bigWordDisplay = document.querySelector("#big-word-display")
+    
+}
