917a0ee356db9ae307fd459f366153061147dcbb
cesmiley <catherinesmiley@gmail.com>
2020-07-28 23:01:49 -0400

    create createNewGame function

--- front_end/index.js
+++ front_end/index.js
@@ -1,12 +1,12 @@
 const BASE_URL = "http://localhost:3000"
 
 document.addEventListener("DOMContentLoaded", () => {
-    renderFirstWord()
+    // add createNewGame
 })
 
 function renderFirstWord() {
     // change to renderBigWords/make separate function?
-    // add clear form function
+    // add clear form/page function
     let bigWordDisplay = document.querySelector("#big-word-display")
     fetch(`${BASE_URL}/big_words`)
     .then(resp => resp.json())
@@ -18,3 +18,9 @@ function renderFirstWord() {
         bigWordDisplay.innerHTML = bigWordsHTML
         })
     }
+
+function createNewGame() {
+    // location.reload(); // check to see if this works
+    let newGameLink = document.querySelector("#new-game")
+    newGameLink.addEventListener("click", console.log("hi"))
+}
